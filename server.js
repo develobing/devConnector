158f3678747246f5168c32f1320c5a5c2b4ac518
Robin <philoin88@gmail.com>
2021-12-19 18:21:36 +0900

    Set up a connection to mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 const PORT = process.env.PORT || 5005;
+
+// Connect Database
+connectDB();
 
 app.get('/', (req, res) => {
   res.send('API running');
