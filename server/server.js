799f4d83dcf1e54c9336c020861d2ee490dbdb18
Robin <philoin88@gmail.com>
2021-12-20 22:44:35 +0900

    Change project structure

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const connectDB = require('./config/db');
+const connectDB = require('./middleware/db');
 
 const app = express();
 const PORT = process.env.PORT || 5005;
