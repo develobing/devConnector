8ad32a9c5658b535abc6eb1c96a0d60190c15c70
Robin <philoin88@gmail.com>
2021-12-20 23:46:39 +0900

    Working on client setting for sign up and login

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const connectDB = require('./middleware/db');
 
 const app = express();
-const PORT = process.env.PORT || 5005;
+const PORT = process.env.PORT || 3005;
 
 // Connect Database
 connectDB();
