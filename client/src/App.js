735c4e82791a3a6e89afcba1dfc3a0d0f5a7ade4
Robin <philoin88@gmail.com>
2021-12-28 00:03:57 +0900

    Working on Profile

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Login from './components/auth/Login';
 import Register from './components/auth/Register';
 import Profiles from './components/profiles/Profiles';
 import Dashboard from './components/dashboard/Dashboard';
+import Profile from './components/profile/Profile';
 import CreateProfile from './components/profile-forms/CreateProfile';
 import EditProfile from './components/profile-forms/EditProfile';
 import AddExperience from './components/profile-forms/AddExperience';
@@ -40,6 +41,7 @@ const App = () => {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <Route exact path="/profiles" component={Profiles} />
+              <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute
                 exact
--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -63,7 +63,7 @@ export const getProfileById = (userId) => async (dispatch) => {
 // Get github repos
 export const getGithubRepos = (username) => async (dispatch) => {
   try {
-    const res = await axios.get(`/api/profile/github${username}`);
+    const res = await axios.get(`/api/profile/github/${username}`);
 
     dispatch({
       type: GET_REPOS,
